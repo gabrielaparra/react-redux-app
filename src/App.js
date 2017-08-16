7f0c36da123f5cddbc3cd19d330db3746404ab0e
gparra <gparra@ip-192-168-0-110.ec2.internal>
2017-08-16 17:51:25 -0400

    created todo reducer

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ class App extends Component {
       <div className="App">
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React withsdl Redux</h2>
+          <h2>Welcome to React with Redux</h2>
         </div>
         <div className="Todo-App">
           <form>
